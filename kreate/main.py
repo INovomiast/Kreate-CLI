c1f6acebecc0ace7f6cc444d20775b7c59f9c60e
inovomiast <inovomiast@hotmail.com>
2022-04-20 12:42:14 +0200

    We've started!

---
+++ kreate/main.py
@@ -0,0 +1,47 @@
+#Import Click Lib
+import click
+
+#Import Tabulate
+import tabulate
+
+#Import Kreate_Create Modules
+
+
+#Functions
+def lang_table():
+    table_data = [
+        ['Python', 'Supported'],
+        ['Javascript', 'Supported'],
+        ['NodeJS', 'Supported'],
+    ]
+    
+    print(tabulate.tabulate(table_data, headers=["Lang", "Support"]))
+
+def create_prj():
+    #Step1:
+    prj_name = input("Project Name: ")
+    #Step2:
+    
+
+
+#Create Click Group
+@click.group()
+def cli():
+    pass
+
+# --langs
+@click.command()
+@click.option('--langs', default=1, help='Display a Language Support Table')
+def langs(langs):
+    click.echo(lang_table())
+    
+# --create-project
+@click.command()
+@click.option('--c', default=1, help='Create a new project!')
+def create(create):
+    click.echo('Create Command')
+    
+cli.add_command(langs)
+cli.add_command(create)
+
+cli()
